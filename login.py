bd8f9cd071d8a4e9c4f0bb4f5191d85a6cded8ef
raman <raman.bbsbec@gmail.com>
2025-02-11 20:55:55 +0530

    Enhance login with authentication

--- login.py
+++ login.py
@@ -2,3 +2,10 @@ def login():
     print("User login functionality")
 
 login()
+
+ if username == "admin" and password == "admin123":
+        print("Login Successful")
+    else:
+        print("Invalid Credentials")
+
+login("admin", "admin123")
